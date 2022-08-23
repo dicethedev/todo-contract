2d113929ac4bea829b43a5955fa893289bbf75b6
dicetheDev <samuelblessing125@gmail.com>
2022-08-23 21:38:04 +0100

    initial commit

--- contracts/IDonation.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-interface IDonation {
-
-     function donate() payable external;
-     function withdraw(uint256 amount) external returns (bool);
-}
